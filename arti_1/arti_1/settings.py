d1c7fff3685a7a9e62d1caef6c04ef262b177429
Mark NeuCollins <mark.neucollins@emory.edu>
2024-02-19 08:29:57 -0500

    adds OpenSans font

--- arti_1/arti_1/settings.py
+++ arti_1/arti_1/settings.py
@@ -14,7 +14,8 @@ from pathlib import Path
 from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+# BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -121,6 +122,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+    BASE_DIR / 'static' / 'fonts'
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
