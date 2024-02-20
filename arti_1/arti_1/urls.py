c627db002f1db3c7d2912652b6a89eca42d26fa2
Mark NeuCollins <mark.neucollins@emory.edu>
2024-02-19 21:28:34 -0500

    first working page

--- arti_1/arti_1/urls.py
+++ arti_1/arti_1/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('', include('arti_1_app.urls')),  # Include the URLs of your app
 ]
